86a7edd95e23566f40906f542a99bfeb8d49d83f
aeh1707d <abdelhadi.hireche@outlook.com>
2022-09-09 00:25:50 +0100

    query and update data

--- queries.sql
+++ queries.sql
@@ -8,3 +8,32 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE round(weight_kg::numeric, 2) >= 10.23 AND round(weight_kg::numeric, 2) <= 17.3;
+
+BEGIN;
+update animals set species = 'unspecified';
+rollback;
+-- 
+BEGIN;
+update animals set species = 'digmon' where name like '%mon';
+update animals set species = 'pokemon' where species is NULL;
+commit;
+-- 
+BEGIN;
+delete from animals;
+rollback;
+
+BEGIN;
+delete from animals where date_of_birth > 'JAN 01, 2022';
+SAVEPOINT s1;
+update animals set weight_kg = -1 * weight_kg;
+rollback to s1;
+update animals set weight_kg = -1 * weight_kg where weight_kg < 0;
+commit;
+
+-- spicies questions 
+select COUNT(*) from animals;
+select COUNT(*) from animals where escape_attempts = 0;
+select avg(weight_kg) from animals;
+select neutered, count(*) from animals group by neutered;
+select min(weight_kg), max(weight_kg) from animals group by species;
+select species, avg(escape_attempts) from animals where date_of_birth between 'JAN 01, 1990' and 'DEC 31, 2000' group by species;
