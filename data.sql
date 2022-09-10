6a36b4a1ad8616918107825349fecbe4fa72658f
aeh1707d <abdelhadi.hireche@outlook.com>
2022-09-10 19:53:31 +0100

    Populates and update data

--- data.sql
+++ data.sql
@@ -6,3 +6,17 @@ INSERT INTO animals VALUES(3,'Pikachu','Jan 07, 2021',1,false,15.04);
 INSERT INTO animals VALUES(4,'Devimon','May 12, 2017',5,true,11);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander','Feb 08, 2020',0,false,-11),('Plantmon','Nov 15, 2021',2,true,-5.7), ('Squirtle','Apr 02, 1993',3,false,-12.13), ('Angemon','Jun 12, 2005',1,true,-45), ('Boarmon','Jun 07, 2005',7,true,20.4), ('Blossom','Oct 13, 1998',3,true,17), ('Ditto','May 14, 2022',4,true,22);
+
+-- owners
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith',34),('Jennifer Orwell',19), ('Bob',45),('Melody Pond',77),('Dean Winchester',14),('Jodie Whittaker',38);
+
+-- spicies
+
+INSERT INTO species (name) VALUES ('Pokemon'),('Digimon');
+
+-- 
+
+update animals set species_id = 2 where name like '%mon';
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
